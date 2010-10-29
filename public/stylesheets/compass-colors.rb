edb5bc65763fe45536b481d6d3df54145e6036f8
Guillaume Bort <guillaume.bort@gmail.com>
2010-10-29 16:00:05 +0200

    Remove included framework since they are never updated. It's better to let applications use any framework version they want.

--- public/stylesheets/compass-colors.rb
+++
@@ -1,7 +0,0 @@
-module Compass
-  module Colors
-  end
-end
-
-require File.join(File.dirname(__FILE__), 'compass/colors', 'hsl')
-require File.join(File.dirname(__FILE__), 'compass/colors', 'sass_extensions')
